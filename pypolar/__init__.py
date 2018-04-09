5623525cda0202f2c0194b4ebd5b2735ba34bdf6
Work Prahl <prahl@bme.ogi.edu>
2018-04-09 09:58:06 -0700

    packaging changes

---
+++ pypolar/__init__.py
@@ -0,0 +1,14 @@
+"""
+================================================================================
+pypolar: Design and Analysis of Polarization
+================================================================================
+
+    http://github.com/scottprahl/pypolar
+"""
+
+__author__ = 'Scott Prahl'
+__version__ = '0.1.0'
+
+from pypolar.basics import *
+from pypolar.interpret import *
+from pypolar.visualization import *
